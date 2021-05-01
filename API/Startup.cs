a3425a4d64df3e63cbb16ea1ceb761a126df6234
lalitgaikwad <lalitgaikwad4@gmail.com>
2021-05-02 02:18:52 +0530

    end of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,7 +36,9 @@ namespace API
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -56,6 +58,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
