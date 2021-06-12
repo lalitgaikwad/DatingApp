f8ca4feb0895ac63471bb624539225847382d7c4
lalitgaikwad <lalitgaikwad4@gmail.com>
2021-06-12 12:40:07 +0530

    end of sect 6

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { MemberDetailComponent } from './member/member-detail/member-detail.component';
+import { MemberListComponent } from './member/member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'members', component: MemberListComponent,canActivate:[AuthGuard]},
+  {path:'members/:id', component: MemberDetailComponent,canActivate:[AuthGuard]},
+  {path:'lists', component: ListComponent,canActivate:[AuthGuard]},
+  {path:'messages', component: MessagesComponent,canActivate:[AuthGuard]},
+  {path:'**', component: HomeComponent, pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,13 +10,23 @@ import { NavComponent } from './nav/nav.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './member/member-list/member-list.component';
+import { MemberDetailComponent } from './member/member-detail/member-detail.component';
+import { ListComponent } from './list/list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -24,8 +34,8 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
-        
+    SharedModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
